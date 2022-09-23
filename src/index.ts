44ffdd90fd46d7d8fdad6d3b2c0d9b36ff8d4cf6
AlexC-ux <eunv2004@gmail.com>
2022-09-24 01:22:23 +0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,9 @@ const nodemailer = require("nodemailer");
 
 var mysql = require('mysql2');
 const urlencodedParser = express.urlencoded({ extended: false });
+
 expressApp.use(cors())
+
 const pool = mysql.createPool({
   connectionLimit: 80,
   host: '141.8.195.33',

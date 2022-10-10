fc2be316c12e4eaea65944e336a5b7cd98aaf835
AlexC-ux <eunv2004@gmail.com>
2022-10-10 22:21:22 +0300

    123

--- src/index.ts
+++ src/index.ts
@@ -20,11 +20,31 @@ var mysql = require('mysql2');
 const urlencodedParser = express.urlencoded({ extended: false });
 
 var corsOptions = {
-  origin: ["localhost"],
+  origin: "*",
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
 expressApp.use(cors(corsOptions))
+// Add headers before the routes are defined
+// Add headers before the routes are defined
+expressApp.use(function (req:any, res:any, next:any) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 const pool = mysql.createPool({
   connectionLimit: 80,

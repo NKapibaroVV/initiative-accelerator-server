df9c6afd26bebb0ccb152171fd24e82eba4dd4e6
AlexC-ux <eunv2004@gmail.com>
2022-10-12 00:38:30 +0300

    123

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ expressApp.use(function (req: any, res: any, next: any) {
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', false);
 
   // Pass to next layer of middleware
   next();

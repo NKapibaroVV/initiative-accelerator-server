3a3b46ecb838b1cd28f1b07252499ec70bf39f8d
AlexC-ux <eunv2004@gmail.com>
2022-10-16 11:25:52 +0300

    123

--- src/index.ts
+++ src/index.ts
@@ -186,7 +186,7 @@ expressApp.post("/api/get_taken_initiatives/", (req: any, res: any) => {
       res.send(err.message)
     } else {
       let user = result[0];
-      let sql = `SELECT * FROM \`initiatives_taken\` INNER JOIN \`initiatives\` on \`initiatives_taken\`.\`initiative_id\`=\`initiatives\`.\`id\` WHERE user_id='${user.id}'`
+      let sql = `SELECT * FROM \`initiatives_taken\` INNER JOIN \`initiatives\` on \`initiatives_taken\`.\`initiative_id\`=\`initiatives\`.\`id\` INNER JOIN \`initiative_conversations\` ON \`initiative_conversations\`.\`initiative_id\`=\`initiatives\`.\`id\` WHERE user_id='${user.id}'`
       pool.query(sql, function (err: any, result: any) {
         if (err) {
           res.send(err.message)

8da5296c7bf26f7925e4b68325a8d548cab82a61
AlexC-ux <eunv2004@gmail.com>
2022-10-25 11:49:32 +0300

    123

--- src/index.ts
+++ src/index.ts
@@ -628,13 +628,7 @@ expressApp.post("/api/get_initiative_members/", (req: any, res: any) => {
                 });
                 taken = removeEmpty(taken);
                 completed = removeEmpty(completed);
-                if (Object.keys(taken).length>0&&Object.keys(completed).length>0) {
-                  res.send([...taken, ...completed])
-                }else if(Object.keys(taken).length>0){
-                  res.send(taken)
-                }else{
-                  res.send(completed)
-                }
+                res.send([...completed,...taken])
               }
             })
           }

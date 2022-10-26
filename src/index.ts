e9bf9ee16ffb5c5b7eda2afa02a597f9d06de53e
AlexC-ux <eunv2004@gmail.com>
2022-10-26 17:21:11 +0300

    123

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ expressApp.use(function (req: any, res: any, next: any) {
 
 
 const pool = mysql.createPool({
-  connectionLimit: 10,
+  connectionLimit: 200,
   host: 'f0711974.xsph.ru',
   user: 'f0711974_initiative_accelerator',
   password: process.env.DB_PASSWORD,

20ea51cdb5abc699630080a3934a14bf4b6fad17
AlexC-ux <eunv2004@gmail.com>
2022-10-30 13:24:17 +0300

    123

--- src/index.ts
+++ src/index.ts
@@ -467,9 +467,26 @@ expressApp.post("/api/add_initiative/", (req: any, res: any) => {
                       } else {
                         res.send(result);
                         if (!isPrivate) {
+                          console.log(`
+                          В *[акселераторе инициатив](https://initiative-accelerator-front-alexc-ux.vercel.app/cab/)* новое задание \!
+                          Название: *${telegramBot.escapeMarkdown(title) }*
+                          Категория:*${telegramBot.escapeMarkdown(category)}*
+                          Мест: *${!!users_limit ? users_limit : "Не ограничено"}*
+                          Можно начать выполнять до: *${!!take_deadline ? new Date(take_deadline).toLocaleString() : "Не ограничено"}*
+                          Нужно выполнять до: *${!!complete_deadline ? new Date(complete_deadline).toLocaleString() : "Не ограничено"}*
+                          Награда: *${income} баллов*
+                          
+                          *Описание:*
+                          ||${telegramBot.escapeMarkdown(content)}||
+                          
+                          Для того чтобы принять участие в этом задании перейдите в [личный кабинет](https://initiative-accelerator-front-alexc-ux.vercel.app/cab/).
+                          
+                          P.S.
+                          ||Задание будет отображаться у всех пользователей до тех пор, пока к его выполнению возможно приступить.||
+                          `)
                           tgBot.sendMessage("@mospedreserv",
                             `
-В *[акселераторе инициатив](https://initiative-accelerator-front-alexc-ux.vercel.app/cab/)* новое задание\!
+В *[акселераторе инициатив](https://initiative-accelerator-front-alexc-ux.vercel.app/cab/)* новое задание \!
 Название: *${telegramBot.escapeMarkdown(title) }*
 Категория:*${telegramBot.escapeMarkdown(category)}*
 Мест: *${!!users_limit ? users_limit : "Не ограничено"}*

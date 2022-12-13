5853cfe46425a0603f86405015975a58acfd700c
AlexC-ux <eunv2004@gmail.com>
2022-12-13 20:02:21 +0300

    mail upd

--- Mailer/index.js
+++ Mailer/index.js
@@ -2,25 +2,31 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SendServiceEmail = void 0;
 const nodemailer_1 = require("nodemailer");
-let serviceMailTransporter;
-(0, nodemailer_1.createTestAccount)().then(testAccount => {
-    serviceMailTransporter = (0, nodemailer_1.createTransport)({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false,
-        auth: {
-            user: testAccount.user,
-            pass: testAccount.pass, // generated ethereal password
-        },
-    });
-});
 class SendServiceEmail {
+    static getTransport() {
+        return new Promise(() => {
+            (0, nodemailer_1.createTestAccount)().then(testAccount => {
+                let Transport = (0, nodemailer_1.createTransport)({
+                    host: "smtp.ethereal.email",
+                    port: 587,
+                    secure: false,
+                    auth: {
+                        user: testAccount.user,
+                        pass: testAccount.pass, // generated ethereal password
+                    },
+                });
+                return Transport;
+            });
+        });
+    }
     static sendText(params) {
-        serviceMailTransporter.sendMail({
-            from: process.env.SERVICE_EMAIL,
-            to: params.recipient,
-            subject: params.subject,
-            text: `${params.text}\n\n\n\nЭто сообщение является системным. Пожалуйста, не отвечайте на него.`
+        this.getTransport().then((serviceMailTransporter) => {
+            serviceMailTransporter.sendMail({
+                from: process.env.SERVICE_EMAIL,
+                to: params.recipient,
+                subject: params.subject,
+                text: `${params.text}\n\n\n\nЭто сообщение является системным. Пожалуйста, не отвечайте на него.`
+            });
         });
     }
 }
--- src/Mailer/index.ts
+++ src/Mailer/index.ts
@@ -1,31 +1,37 @@
 
-import mailer, { Transporter, createTransport, createTestAccount, TestAccount } from "nodemailer";
+import mailer, { Transporter, createTransport, createTestAccount } from "nodemailer";
 import { MailOptions } from "nodemailer/lib/json-transport";
 import { isPropertySignature } from "typescript";
 
-let serviceMailTransporter:Transporter;
 
-createTestAccount().then(testAccount=>{
-  serviceMailTransporter = createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  });
-});
 
 
 
 export class SendServiceEmail{
+
+  private static getTransport():Promise<Transporter>{
+    return new Promise(()=>{
+      createTestAccount().then(testAccount=>{
+        let Transport:Transporter = createTransport({
+          host: "smtp.ethereal.email",
+          port: 587,
+          secure: false, // true for 465, false for other ports
+          auth: {
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass, // generated ethereal password
+          },
+        });
+        return Transport
+      })
+    });    
+  }
+
     public static sendText(params:{recipient:string,subject:string,text:string}) {
-        serviceMailTransporter.sendMail({
-            from:process.env.SERVICE_EMAIL,
-            to:params.recipient,
-            subject:params.subject,
-            text:`${params.text}\n\n\n\nЭто сообщение является системным. Пожалуйста, не отвечайте на него.`
-        })
+      this.getTransport().then((serviceMailTransporter)=>{serviceMailTransporter.sendMail({
+        from:process.env.SERVICE_EMAIL,
+        to:params.recipient,
+        subject:params.subject,
+        text:`${params.text}\n\n\n\nЭто сообщение является системным. Пожалуйста, не отвечайте на него.`
+    })});
     }
 }

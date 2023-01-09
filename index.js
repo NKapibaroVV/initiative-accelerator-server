04479a3a86529f7302729401c98508506a137e52
AlexC-ux <eunv2004@gmail.com>
2023-01-09 17:42:06 +0300

    update /api/get_shop_item_users/

--- index.js
+++ index.js
@@ -325,15 +325,20 @@ expressApp.post(`/api/get_shop_item_users/`, (req, res) => {
                     }
                     else {
                         let shopItem = resultShopItem[0];
-                        let query = `SELECT * from \`shop_items\` JOIN \`users\` ON \`shop_items\`.\`user_id\`=\`users\`.\`id\`  WHERE \`shop_items\`.\`title\`='${shopItem.title}' AND \`shop_items\`.\`cost\`=${shopItem.cost} AND \`shop_items\`.\`description\`='${shopItem.description}' AND \`shop_items\`.\`deadline_take\`=${shopItem.deadline_take} AND \`shop_items\`.\`users_limit\`=${shopItem.users_limit}`;
-                        pool.query(query, function (err, result) {
-                            if (err) {
-                                res.send({ 0: err, 1: query });
-                            }
-                            else {
-                                res.send(result);
-                            }
-                        });
+                        if (shopItem.user_id != null) {
+                            let query = `SELECT * from \`shop_items\` JOIN \`users\` ON \`shop_items\`.\`user_id\`=\`users\`.\`id\`  WHERE \`shop_items\`.\`title\`='${shopItem.title}' AND \`shop_items\`.\`cost\`=${shopItem.cost} AND \`shop_items\`.\`description\`='${shopItem.description}' AND \`shop_items\`.\`deadline_take\`=${shopItem.deadline_take} AND \`shop_items\`.\`users_limit\`=${shopItem.users_limit}`;
+                            pool.query(query, function (err, result) {
+                                if (err) {
+                                    res.send({ 0: err, 1: query });
+                                }
+                                else {
+                                    res.send(result);
+                                }
+                            });
+                        }
+                        else {
+                            res.send("Никому не назначено");
+                        }
                     }
                 });
             }
